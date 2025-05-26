00ea679bc07027d1ab9a9044cb2ec1875d516431
fxfuren <nikitostimoshenko@gmail.com>
2025-05-26 17:11:44 +0300

    Refactor Block5 component to replace Card layout with a map and contact information for enhanced user interaction

--- src/components/Block5.tsx
+++ src/components/Block5.tsx
@@ -1,16 +1,38 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+'use client'
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps'
 
 export default function Block5() {
 	return (
-		<Card className='bg-gray-50 text-center'>
-			<CardHeader>
-				<CardTitle>Готовы увеличить продажи?</CardTitle>
-			</CardHeader>
-			<CardContent>
-				<p className='mb-4'>Оставьте заявку, и мы свяжемся с вами</p>
-				<Button>Оставить заявку</Button>
-			</CardContent>
-		</Card>
+		<div className="relative w-full h-[500px]">
+			<YMaps>
+				<Map
+					defaultState={{
+						center: [54.0006, 27.2976],
+						zoom: 14,
+						controls: ['zoomControl'],
+					}}
+					modules={['control.ZoomControl']}
+					width="100%"
+					height="100%"
+				>
+					<Placemark
+						geometry={[54.0006, 27.2976]}
+						options={{ preset: 'islands#blueIcon' }}
+						properties={{
+							balloonContentHeader: 'Давай сделаем',
+							balloonContentBody: 'г. Заславль, ул.Набережная-1 к.305',
+						}}
+					/>
+				</Map>
+			</YMaps>
+
+			<div className="absolute top-10 left-10 bg-white shadow-xl rounded-xl p-6 w-[320px] max-w-full z-10">
+				<h2 className="text-2xl font-bold mb-2">Контакты</h2>
+				<p className="text-sm mb-1">+375 (25) 990-58-88</p>
+				<p className="text-sm mb-1 text-blue-600">davay_sdelaem@mail.ru</p>
+				<p className="text-sm mb-1">г. Заславль ул.Набережная-1 к.305</p>
+				<p className="text-sm">ПН-выходной, С 11:00 до 20:00</p>
+			</div>
+		</div>
 	)
 }
