1c0014cce97544f6caee9bf3c9df7398d2fcbcab
fxfuren <nikitostimoshenko@gmail.com>
2025-06-02 11:58:55 +0300

    Enhance metadata with additional keywords and Yandex verification for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Head from 'next/head'
 import './globals.css'
 
 const geistSans = Geist({
@@ -16,6 +17,28 @@ export const metadata: Metadata = {
 	title: 'Давай сделаем',
 	description:
 		'Надежный ремонт телефонов, планшетов и ноутбуков в Заславле с гарантией',
+	keywords: [
+		'Ремонт цифровой техники',
+		'Ремонт смартфонов в Заславле',
+		'Сервисный центр по ремонту ноутбуков',
+		'Замена экрана телефона',
+		'Восстановление данных с жесткого диска',
+		'Ремонт планшетов недорого',
+		'Услуги по ремонту компьютеров',
+		'Срочный ремонт электроники',
+		'Ремонт зарядных устройств',
+		'Чистка и профилактика ноутбуков',
+		'Устранение вирусов и malware',
+		'Настройка и ремонт Wi-Fi',
+		'Ремонт видеокарт и комплектующих',
+		'Замена батареи на смартфоне',
+		'Ремонт принтеров и МФУ',
+		'Обновление ПО и прошивка',
+		'Ремонт телевизоров и мониторов',
+		'Диагностика неисправностей техники',
+		'Ремонт бытовой электроники',
+		'Гарантия на ремонт цифровой техники',
+	],
 	icons: {
 		icon: '/img/logo.jpg',
 	},
@@ -48,6 +71,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='ru'>
+			<Head>
+				<meta name='yandex-verification' content='80af4c02ce371e06' />
+			</Head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
